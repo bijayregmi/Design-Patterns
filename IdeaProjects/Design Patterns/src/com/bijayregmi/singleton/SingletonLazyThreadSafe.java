153b3cfc7adfc0c30787d0024d9a60c2ff2cf0a8
Bijay Regmi <er.bijayregmi@gmail.com>
2017-06-14 13:02:43 -0400

    Thread safe singleton class

--- IdeaProjects/Design Patterns/src/com/bijayregmi/singleton/SingletonLazyThreadSafe.java
+++ IdeaProjects/Design Patterns/src/com/bijayregmi/singleton/SingletonLazyThreadSafe.java
@@ -3,5 +3,26 @@ package com.bijayregmi.singleton;
 /**
  * Created by bijayregmi on 6/14/17.
  */
-public class SingletonLazyThreadSafe {
+public final class SingletonLazyThreadSafe {
+
+    private static volatile SingletonLazyThreadSafe INSTANCE = null;
+
+    public long licesenceNumber ;
+
+    private SingletonLazyThreadSafe(){}
+
+    public static SingletonLazyThreadSafe getINSTANCE(){
+
+
+        if(INSTANCE == null){
+            synchronized (SingletonLazyThreadSafe.class){
+                if(INSTANCE == null){
+                    INSTANCE = new SingletonLazyThreadSafe();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
 }
